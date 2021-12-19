b6e05def448c3888b040f353136c9c6edad60643
Nehoray_Developer <66194934+NEDev2@users.noreply.github.com>
2021-12-19 06:29:35 +0000

    Added headless mode!

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ chrome_options = Options()
 chrome_options.add_argument("--start-maximized")
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-#chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 
 chat_id = 2129581818
 
