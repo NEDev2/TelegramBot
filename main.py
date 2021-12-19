b5108d3de4f9103020b55d0966e733172af29a61
Nehoray_Developer <66194934+NEDev2@users.noreply.github.com>
2021-12-19 06:26:42 +0000

    ....

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 chrome_options = Options()
 chrome_options.add_argument("--start-maximized")
 chrome_options.add_argument('--no-sandbox')
